02415c815c1b592b7c1a9a7ba5108fb463851632
Bonnabelle <bonnabelleg@gmail.com>
2016-08-26 13:23:09 -0500

    Fixed everything.

--- caesar/main.py
+++ caesar/main.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import cgi
 
@@ -24,6 +8,7 @@ top = """
     <title>The Rotater</title>
 </head>
 <body>
+<h1>Caesar Encryption</h1>
 """
 
 bottom = """
@@ -31,58 +16,41 @@ bottom = """
 </html>
 """
 
-big_title = """<h1>Caesar Encryption</h1>"""
+caesar = """
+<form method="post">
+    <label> Enter rotation amount.
+        <input type="number" name="rot" value="0">
+    </label>
+    <br>
+    <textarea rows="10" cols="50" type="text" name="message">
+    %(entered)s
+    </textarea>
+    <br>
+    <input type="submit"></form>
+"""
 
-def rot_custom(rot,message):
-    final = ""
-    message = str(message)
-    for char in message:
-        if char.isalpha() == False:
-            final += char
-        letter = (ord(char) + int(rot)) % 26
-        letter = chr(letter)
-        final += letter
-    return final
+from caesar import encrypt
 
 class Home(webapp2.RequestHandler):
 
-    def post(self):
-        #This is all over the place
-        #num = self.request.get("num")
-
-        entered = ""
-
-        caesar = """
-        <form method="post" action="/">
-            <label> Enter rotation amount.
-                <input type="number" name="rot" value="0">
-            </label>
-            <br>
-            <textarea rows="10" cols="50" type="text" name="message">
-            %(entered)s
-            </textarea>
-            <br>
-            <input type="submit">
-        </form>
-        """
-        final = top + big_title + caesar + bottom
-        #final = rot_custom(rot,message)
-
+    def get(self):
+        final = top + caesar + bottom
         self.response.write(final)
+
+    def post(self):
 
         rot = self.request.get("rot")
         rot = int(rot)
         message = self.request.get("message")
         message = cgi.escape(message, quote=True)
 
-        encryption = rot_custom(rot,message)
+        entered = {"entered": encrypt(message,rot)}
 
-        entered += encryption
+        new_caesar = caesar % entered
 
-        encrypted_final = top + str(encryption) + bottom
+        encrypted_final = top + new_caesar + bottom
 
         self.response.write(encrypted_final)
-
 
 app = webapp2.WSGIApplication([
     ('/', Home)
